9e647232671296b199c06b6910659dd561cac420
Rishabh-creator601 <75622687+Rishabh-creator601@users.noreply.github.com>
2021-09-20 12:59:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from main import app
+import os 
 
 if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
 	app.secret_key = 'FLASKQRCODE'
-	app.run()
+	app.run(host='0.0.0.0', port=port, debug=True)
